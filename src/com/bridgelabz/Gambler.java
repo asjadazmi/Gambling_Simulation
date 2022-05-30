bc37df73f5a5d8d86a6bf5cc608d9f4fdd4473d4
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-30 21:39:59 +0530

    Calculative Gambler if won or lost 50% of the stake

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -22,7 +22,23 @@ public class Gambler {
 			System.out.println("gambler stake amount is:"+stake_amount);
 			break;
 		default:
+			int win_amount=150;
+			int loose_amount=50;
+			while(stake_amount > loose_amount && stake_amount < win_amount) {
+				int bet=(int)Math.floor(Math.random()*10)%2;
+				switch(bet) {
+				case 1:
+					System.out.println("Bet Win");
+	                System.out.println("Final Amount: "+(stake_amount += bet_amount));
+	                break;
+				case 0:
+					 System.out.println("Bet Loss");
+		             System.out.println("Final Amount: "+(stake_amount -= bet_amount));
+		             break;
+		         default:
+		          }
 		}
-		}
+	}
+	}
 	}
 

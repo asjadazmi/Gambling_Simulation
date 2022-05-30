6652f461954280728ab31a1c7c0911692e33dc78
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-30 21:00:03 +0530

    Added: Welcome to the gambling simulation

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+import java.util.Random;
+public class Gambler {
+	public static void main(String[] args) {
+		game();
+	}
+	public static void game() {
+		System.out.println("welcome to the gambling simulation:😂");
+	}
+}

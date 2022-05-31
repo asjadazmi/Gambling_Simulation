3cb9e5b37e2192faeb0aecaf49d177f1bcb2a96d
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 11:11:45 +0530

    Added:Calculatin each days in the month

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -16,6 +16,9 @@ public class Gambler {
 			int win_amount=100+50;
 			int loose_amount=100-50;
 			int winner=0;
+			int Winning = 0;
+	        int Winner_Day=0;
+	        int Losser_Day=0;
 			
 			for(int day=1;day<=20;day++) {
 				stake_amount=100;
@@ -40,10 +43,24 @@ public class Gambler {
 	                System.out.println("you are looser "+day);
 	                winner -= 50;
 	            }
+				if(stake_amount==win_amount) {
+					Winner_Day+=1;
+					winner+=loose_amount;
+				}
+				else {
+					Losser_Day-=1;
+					winner -=50;
+					
+				}
+				System.out.println("Total Win Days in month: "+Winner_Day);
+		        System.out.println("Total Loss Days in month: "+Losser_Day);
+		        System.out.println("Total Win Price Won In Month Is " +winner );	
+					
+				}
 	            System.out.println("Total Win Price: "+winner );
 			}
 		}
 	
-	}
+	
 	
 

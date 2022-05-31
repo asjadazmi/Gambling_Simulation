d9248244306873cfb38bfd76d5a40d4e330cf09f
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 10:57:53 +0530

    Added:Everday win or loose amount

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -12,33 +12,38 @@ public class Gambler {
 		System.out.println("betting of every game is: "+bet_amount+"$");
 		int check=(int)Math.floor(Math.random()*10)%2;
 		System.out.println("ran: "+check);
-		switch(check) {
-		case 1:
-			stake_amount+=bet_amount;
-		    System.out.println("gambler stake amount is:"+stake_amount);
-		    break;
-		case 0:
-			stake_amount-=bet_amount;
-			System.out.println("gambler stake amount is:"+stake_amount);
-			break;
-		default:
-			int win_amount=150;
-			int loose_amount=50;
-			while(stake_amount > loose_amount && stake_amount < win_amount) {
-				int bet=(int)Math.floor(Math.random()*10)%2;
-				switch(bet) {
-				case 1:
-					System.out.println("Bet Win");
-	                System.out.println("Final Amount: "+(stake_amount += bet_amount));
-	                break;
-				case 0:
-					 System.out.println("Bet Loss");
-		             System.out.println("Final Amount: "+(stake_amount -= bet_amount));
-		             break;
-		         default:
-		          }
+		
+			int win_amount=100+50;
+			int loose_amount=100-50;
+			int winner=0;
+			
+			for(int day=1;day<=20;day++) {
+				stake_amount=100;
+				while(stake_amount > loose_amount && stake_amount < win_amount) {
+					int check1=(int)Math.floor(Math.random()*10)%2;
+					switch(check1) {
+					case 1:
+						stake_amount+=bet_amount;
+						break;
+					case 0:
+						stake_amount-=bet_amount;
+						break;
+					 default:
+					}
+				}
+				if(stake_amount == win_amount) {
+	                System.out.println("you won the game "+day);
+	                winner += 50;
+	            }
+				else 
+	            {
+	                System.out.println("you are looser "+day);
+	                winner -= 50;
+	            }
+	            System.out.println("Total Win Price: "+winner );
+			}
 		}
+	
 	}
-	}
-	}
+	
 
